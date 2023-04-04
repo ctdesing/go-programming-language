781265cd34727b2ec97d3471ef30a615f4e952fe
Miguel Arias <ctdesing@gmail.com>
2023-04-04 13:16:22 -0400

    1.3

--- 1.3_dup.go
+++ 1.3_dup.go
@@ -8,11 +8,40 @@ import (
 
 func dup() {
 	counts := make(map[string]int)
-	input := bufio.NewScanner(os.Stdin)
+	files := os.Args[1:]
+	if len(files) == 0 {
+		countLines(os.Stdin, counts)
+		printRepeats(counts)
+		return
+	}
+
+	for _, arg := range files {
+		f, err := os.Open(arg)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "dup: %v\n", err)
+			continue
+		}
+
+		countLines(f, counts)
+		f.Close()
+	}
+
+	printRepeats(counts)
+
+}
+
+func countLines(f *os.File, counts map[string]int) {
+	input := bufio.NewScanner(f)
 	for input.Scan() {
+		if input.Text() == "eof" {
+			break
+		}
 		counts[input.Text()]++
 	}
 	// NOTE: ignoring potential errors from input.Err()
+}
+
+func printRepeats(counts map[string]int) {
 	for line, n := range counts {
 		if n > 1 {
 			fmt.Printf("%d\t%s\n", n, line)
